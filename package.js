fe1c591ee8e284c49367d8e681a85d438f827ebc
Luiz de Oliveira <luizcarlos1405@gmail.com>
2021-01-24 19:37:00 -0300

    add api.use('ecmascript') to package.js

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
-  name: "quave:autoformik",
-  version: "0.0.1",
-  summary: "Create Formik forms automatically from a SimpleSchema object.",
-  git: "https://github.com/quavedev/autoformik"
+  name: 'quave:autoformik',
+  version: '0.0.1',
+  summary: 'Create Formik forms automatically from a SimpleSchema object.',
+  git: 'https://github.com/quavedev/autoformik',
 });
 
 Package.onUse(api => {
-  api.versionsFrom("1.10.2");
-  api.use("quave:definitions");
+  api.versionsFrom('1.10.2');
+  api.use('quave:definitions@1.0.0');
+  api.use('ecmascript');
 
-  api.mainModule("autoformik.js");
+  api.mainModule('autoformik.js');
 });
